e0bc6337022b650d48bad1fd41f343b11748108c
arf <goobysc@gmail.com>
2021-01-29 10:41:34 -0500

    finished CSVData methods

--- csv_io/csvdata.py
+++ csv_io/csvdata.py
@@ -15,20 +15,36 @@ class CSVData:
         for entry in self.csv:
             yield entry
 
-    def __check_categories(self, item: str) -> None:
-        if item in self.categories:
-            return None
-        else:
-            raise Exception(f'"{item}" not available in {self.filename}\'s categories:\n{self.categories}')
+    def __check_categories(self, *args) -> None:
+        for item in args:
+            if item in self.categories:
+                return None
+            else:
+                raise Exception(f'"{item}" not available in {self.filename}\'s categories:\n{self.categories}')
 
     def count(self):
         return len(self.csv)
 
     def find(self, category: str, match_value: str) -> dict:
-        pass
+        self.__check_categories(category)
+        for entry in self.csv:
+            if entry[category] == match_value:
+                return entry
+        # if not found, return empty dict
+        return {}
 
     def findall(self, category: str, match_value: str) -> list[dict]:
-        pass
+        self.__check_categories(category)
+        found: list[dict] = []
+        for entry in self.csv:
+            if entry[category == match_value]:
+                found.append(entry)
+        return found
 
     def values(self, category: str, search_category: str, match_value: str) -> list[str]:
-        pass
+        self.__check_categories(category, search_category)
+        found: list[dict] = self.findall(search_category, match_value)
+        if not found:
+            return []
+        else:
+            return [x[category] for x in found]
